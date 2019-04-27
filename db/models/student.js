9613b927ca06fd581da636f0c683b8a988b90605
Michelle Kiss <picturethis56@gmail.com>
2019-04-27 11:34:12 -0500

    01 portion done

--- db/models/student.js
+++ db/models/student.js
@@ -2,7 +2,25 @@
 
 const Sequelize = require('sequelize');
 const db = require('../db');
+// const db = new Sequelize ('postgres://localhost:5432/study-saturdays');
 
-const Student = db.define();
+
+const Student = db.define('student', {
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    }
+  });
 
 module.exports = Student;
