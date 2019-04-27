8fff6ff22ee798853587664233206639f832103b
Michelle Kiss <picturethis56@gmail.com>
2019-04-27 12:54:25 -0500

    router troubles

--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,42 @@
 const router = require('express').Router();
+const Student = require('/db../models../student')
 
+
+
+router.get('/students', async (req, res, next) => {
+    try {
+        const students = await Student.findAll();
+        res.send()
+    } catch (error) { next(error) }
+});
+
+router.get('/students', async (req, res, next) => {
+    try {
+        const students = await Student.findAll();
+        res.send()
+    } catch (error) { next(error) }
+});
+
+router.post('/students', async (req, res, next) => {
+    try {
+        const students = await Student.findAll();
+        res.send()
+    } catch (error) { next(error) }
+});
+
+
+router.put('/students', async (req, res, next) => {
+    try {
+        const students = await Student.findAll();
+        res.send()
+    } catch (error) { next(error) }
+});
+
+router.delete('/students', async (req, res, next) => {
+    try {
+        const students = await Student.findAll();
+        res.send()
+    } catch (error) { next(error) }
+});
 
 module.exports = router;
